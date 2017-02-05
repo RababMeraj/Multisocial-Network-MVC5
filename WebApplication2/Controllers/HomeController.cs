f96d4c121ed27046f5259aeb2a0f47ff97974a90
Moosa Baloch <moosa.bh@gmail.com>
2017-02-06 01:17:09 +0500

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            ViewBag.Message = "Your Sign In.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your application Login page.";
+
+            return View();
+        }
+        public ActionResult policy()
+        {
+            ViewBag.Message = "Terms and conditions";
+
+            return View();
+        }
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Home";
+
+            return View();
+        }
+        public ActionResult msg()
+        {
+            ViewBag.Message = "Terms and conditions";
+
+            return View();
+        }
+        public ActionResult noti()
+        {
+            ViewBag.Message = "Terms and conditions";
+
+            return View();
+        }
+        public ActionResult sett()
+        {
+            ViewBag.Message = "Terms and conditions";
+
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Your profile";
+
+            return View();
+        }
+    }
+}
